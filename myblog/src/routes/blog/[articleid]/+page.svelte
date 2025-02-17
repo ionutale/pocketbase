abab9b9eaa6ff5a793baf909805221d883953b69
Ion Utale <ion.utale@guccidigital.com>
2025-02-17 12:48:42 +0100

    articles look nice now

--- myblog/src/routes/blog/[articleid]/+page.svelte
+++ myblog/src/routes/blog/[articleid]/+page.svelte
@@ -1,19 +1,15 @@
 <script>
-
 	let { data } = $props();
   const article = data;
   console.log(article);
-</script>
 
-<h2>Article {article.id}</h2>
+</script>
 
 <section class="article-body">
   {@html article.body}
 </section>
 
 <style>
-  .article-body {
-    margin-top: 20px;
-  }
+  @import './article-page.css'; /* Import the CSS file */
 
 </style>
